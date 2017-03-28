865c0a3b375b2f9e3af824fdbaeb150571c24661
fzemen <franz.zemen@bhengagement.com>
2017-03-28 18:13:26 -0500

    update

---
+++ epic2-answers/app/components/inventoryList/inventoryList.component.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by fzemen on 3/7/17.
+ */
+
+import {Component, OnInit} from "@angular/core";
+import {InventoryService} from "../../services/inventoryService.service";
+import {IInventoryItem} from "../../shapes/IInventoryItem";
+
+@Component({
+  selector:'inventory-list',
+  templateUrl: 'app/components/inventoryList/inventoryList.component.html',
+  styleUrls: ['app/components/inventoryList/inventoryList.component.css']
+})
+export class InventoryList implements OnInit {
+
+  context:string = 'Jewelry Rack';
+
+  items:IInventoryItem[] = [];
+  currentItem:IInventoryItem;
+
+  constructor(private inventoryService:InventoryService) {
+  }
+
+  ngOnInit() {
+    console.log('Instance of InventoryList initialized');
+    this.inventoryService.getItems('someContext')
+      .then((items: IInventoryItem[]) => {
+        this.items = items;
+      });
+  }
+
+  setCurrentItem(item:IInventoryItem) {
+    this.currentItem = item;
+    this.inventoryService.setCurrentItem(this.currentItem);
+  }
+}
